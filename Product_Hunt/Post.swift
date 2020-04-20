3aafeb12954b64e923f2f23e9e59bf45a279a5f8
Ben Simpson <b.albertsimpson@gmail.com>
2020-04-19 20:43:26 -0700

    Created Post model

---
+++ Product_Hunt/Post.swift
@@ -0,0 +1,19 @@
+//
+//  Post.swift
+//  Product_Hunt
+//
+//  Created by Benjamin Simpson on 4/19/20.
+//  Copyright © 2020 Benjamin Simpson. All rights reserved.
+//
+
+import Foundation
+
+struct Post {
+    // Various properties of a post that we either need or want to display
+    let id: Int
+    let name: String
+    let tagline: String
+    let votesCount: Int
+    let commentsCount: Int
+
+}
